8d66f9d21eec1a0090685d322312ff417c090a27
Glitch (hello-express) <none>
2019-10-27 08:43:32 +0000

    🚀❄️ Checkpoint ./server.js:3016321/744

--- server.js
+++ server.js
@@ -8,11 +8,21 @@ app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-app.get( "/uploads/large.txt", req, res ) {
-  
-}
+app.get( "/uploads/large.txt", function ( req, res ) {
+  const file = new fs.ReadStream('/uploads/large.txt');
+  file.pipe(res);
+  file.on('error', function(err){
+        res.statusCode = 500;
+        res.end("Server Error");
+        console.error(err);
+  });
+  file
+    .on( 'open', () => console.log("open") )
+    .on( 'close', () => console.log("close") )
+ 
+    res.on('close', () => file.destroy() )
+})
 
-// listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
